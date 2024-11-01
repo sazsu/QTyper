2852aac6822d8937beb69708a1cd6d86dfcfd6bd
sazsu <shonniy@yandex.ru>
2024-11-01 14:43:33 +0300

    feat: add local profile page of the application

---
+++ app/local_profile_page.py
@@ -0,0 +1,10 @@
+from PyQt6.QtWidgets import QWidget
+
+
+class LocalProfile(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+        self.initUI()
+
+    def initUI(self):
+        pass
