497446b558167b8c726132151d7d0570a81a0990
sazsu <shonniy@yandex.ru>
2024-11-02 19:49:45 +0300

    feat: adjust geometry, restructure initUI, add change_mode method

--- app/main_window.py
+++ app/main_window.py
@@ -1,30 +1,34 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
 
+from app.config import Config
 from app.local_profile_page import LocalProfile
 from app.test_page import TestPage
 
 
 class MainWindow(QMainWindow):
-	def __init__(self):
+	def __init__(self) -> None:
 		super().__init__()
 		self.initUI()
 
-	def initUI(self):
-		self.pages = QStackedWidget(self)  # add pages here
-
+	def initUI(self) -> None:
 		# maximum resolution available
 		self.setGeometry(QApplication.primaryScreen().availableGeometry())
 
-		self.mode = True  # light mode by default
-
+		self.pages = QStackedWidget(self)  # add pages here
+		self.pages.setGeometry(0, 0, self.width(), self.height())
+		
 		self.pages.addWidget(TestPage(self.pages))
 		self.pages.addWidget(LocalProfile(self.pages))
 
 		self.pages.setCurrentIndex(0)  # test page
 
-		self.pages.setGeometry(0, 0, 1920, 1080)
+		self.pages.widget(0).setGeometry(0, 0, self.width(), self.height())
 
-	def change_mode(self):
+		self.mode = False  # light mode by default
+		self.change_mode()
+
+	def change_mode(self) -> None:
 		self.mode = not self.mode
-		for page in self.pages:
-			page.change_mode()
+		self.setStyleSheet(Config.app_light if self.mode else Config.app_dark)
+		for i in range(self.pages.count()):
+			self.pages.widget(i).change_mode(self.mode)
