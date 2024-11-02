ef2f9feaa733da1a4c8b110380e5d4d121fdb6a7
sazsu <shonniy@yandex.ru>
2024-11-02 19:55:19 +0300

    fix(button): prevent button from activating on keypress

--- app/widgets/base_icon_button.py
+++ app/widgets/base_icon_button.py
@@ -1,6 +1,7 @@
 from PyQt6.QtCore import QSize
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QPushButton, QWidget
+from PyQt6.QtCore import Qt
 
 
 class BaseIconButton(QWidget):
@@ -17,6 +18,8 @@ class BaseIconButton(QWidget):
 		self.btn.setGeometry(0, 0, 75, 75)
 		self.set_icon(True)
 		self.btn.setIconSize(QSize(75, 75))
+		# prevent button from activating on keypress
+		self.btn.setFocusPolicy(Qt.FocusPolicy.NoFocus)
 
 	def set_icon(self, mode: bool):
 		self.btn.setIcon(self.icon_paths[mode])
