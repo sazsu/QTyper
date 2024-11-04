e73127d1d773539f3f8bc028731163858a9fbeed
sazsu <shonniy@yandex.ru>
2024-11-04 20:42:02 +0300

    refactor: adjust import order

--- app/widgets/base_icon_button.py
+++ app/widgets/base_icon_button.py
@@ -1,7 +1,7 @@
 from PyQt6.QtCore import QSize
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QPushButton, QWidget
-from PyQt6.QtCore import Qt
 
 
 class BaseIconButton(QWidget):
