61a6b7d836ce6b892472432a2a8ec5095f146e4c
sazsu <shonniy@yandex.ru>
2024-11-08 16:06:17 +0300

    chore: restructure code using ui classes

--- app/main_window.py
+++ app/main_window.py
@@ -10,30 +10,14 @@ class MainWindow(QMainWindow):
 		super().__init__()
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
-
-		self.ui.pages.addWidget(TestPage(self.ui.pages))
+		test_page = TestPage(self)
+		self.ui.pages.addWidget(test_page)
+		self.ui.pages.setCurrentWidget(test_page)
 		self.mode = False
 		self.change_mode()
-
-	# def initUI(self) -> None:
-	# 	# maximum resolution available
-	# 	self.setGeometry(QApplication.primaryScreen().availableGeometry())
-
-	# 	self.pages = QStackedWidget(self)  # add pages here
-	# 	self.pages.setGeometry(0, 0, self.width(), self.height())
-		
-	# 	self.pages.addWidget(TestPage(self.pages))
-	# 	self.pages.addWidget(LocalProfile(self.pages))
-
-	# 	self.pages.setCurrentIndex(0)  # test page
-
-	# 	self.pages.widget(0).setGeometry(0, 0, self.width(), self.height())
-
-	# 	self.mode = False  # light mode by default
-	# 	self.change_mode()
 
 	def change_mode(self) -> None:
 		self.mode = not self.mode
 		self.setStyleSheet(Config.app_light if self.mode else Config.app_dark)
-		for i in range(self.ui.pages.count()):
-			self.ui.pages.widget(i).change_mode(self.mode)
+		self.ui.pages.widget(0).change_mode(self.mode)
+		# self.ui.pages.widget(1).change_mode(mode)
--- app/test_page.py
+++ app/test_page.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QLabel, QWidget, QButtonGroup
+from PyQt6.QtGui import QIcon
 
 from app.widgets.line_chart import LineChart
 from app.widgets.local_profile_button import LocalProfileButton
@@ -13,64 +14,23 @@ from app.ui.test_page_ui import Ui_Form
 class TestPage(QWidget):
 	def __init__(self, parent):
 		super().__init__(parent=parent)
+		self.main_window = parent
 		self.ui = Ui_Form()
 		self.ui.setupUi(self)
-		print([(w.objectName(), w.geometry() )for w in self.children()])
-		print(self.ui.reset_button.geometry())
-		self.ui.mode_button.setVisible(True)
-		self.initUI()
-		# self.setLayout(self.ui.verticalLayout)
-		self.update()
 
-	def initUI(self):
+		self.text_area = TextArea(self, self)
+		self.chart = LineChart(self)
 		
-		# print(self.ui.reset_button.geometry(), self.ui.reset_button.isVisible())
-		# self.ui.reset_button.move(500, 500)
-		# self.ui.reset_button.setVisible(True)
-		# print(self.ui.reset_button.geometry(), self.ui.reset_button.isVisible())
-
-		# self.ui.mode_button.setVisible(True)
-		# self.ui.local_profile_button.setVisible(True)
-		# self.ui.settings_button.setVisible(True)
-		# self.ui.reset_button = ResetButton(
-		# 	'app/assets/reset_light.svg', 'app/assets/reset_dark.svg', self
-		# )
-		# self.ui.mode_button = ModeButton(
-		# 	'app/assets/mode_light.svg', 'app/assets/mode_dark.svg', self
-		# )
-		# self.ui.local_profile_button = LocalProfileButton(
-		# 	'app/assets/local_profile_light.svg',
-		# 	'app/assets/local_profile_dark.svg',
-		# 	self,
-		# )
-		# self.ui.settings_button = SettingsButton(
-		# 	'app/assets/settings_light.svg', 'app/assets/settings_dark.svg', self
-		# )
-		# self.ui.reset_button.move(200, 0)
-		# self.ui.mode_button.move(300, 0)
-		# self.ui.local_profile_button.move(400, 0)
-		# self.ui.settings_button.move(500, 0)
-
-		self.ui.buttonGroup = QButtonGroup(self)
-		# self.ui.buttonGroup.addButton(self.ui.reset_button)
-		# self.ui.buttonGroup.addButton(self.ui.mode_button)
-		# self.ui.buttonGroup.addButton(self.ui.local_profile_button)
-		# self.ui.buttonGroup.addButton(self.ui.settings_button)
-
-		# self.ui.main_container.addWidget(TextArea(self, self))
+		self.ui.main_container.addWidget(self.text_area)
+		self.ui.main_container.addWidget(self.chart)
 
 	def change_mode(self, mode: bool):
-		# print(self.ui.reset_button.__class__.__name__)
-		for b in self.ui.buttonGroup.buttons():
-			print(b.__class__.__name__)
-			print(b.isVisible(), b.geometry())
-			b.change_mode(mode)
-		# for i in range(self.ui.main_container.count()):
-			# self.ui.main_container.widget(i).change_mode(mode)
+		for button in self.ui.buttonGroup.buttons():
+			button.change_mode(mode)
+		self.chart.change_mode(mode)
 
 	def show_line_chart(self, wpm_arr, acc_arr) -> None:
-		self.text_area.setVisible(False)
+		self.ui.main_container.setCurrentWidget(self.chart)
 		self.text_area.reset_test()
 
 		self.chart.set_values(wpm_arr, acc_arr)
-		self.chart.setVisible(True)
