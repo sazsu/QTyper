bbe31396235694d505d9902b2d8b50a0458878e9
sazsu <shonniy@yandex.ru>
2024-11-15 15:39:27 +0300

    feat: add local profile buttons

---
+++ app/widgets/save_button.py
@@ -0,0 +1,10 @@
+from app.widgets.base_icon_button import BaseIconButton
+
+
+class SaveButton(BaseIconButton):
+	def __init__(self, light_icon_path: str, dark_icon_path: str, parent):
+		super().__init__(light_icon_path, dark_icon_path, parent)
+		self.clicked.connect(self.btn_handler)
+
+	def btn_handler(self):
+		pass
