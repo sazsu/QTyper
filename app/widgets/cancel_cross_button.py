a0256a8232cbe33f627d0f014db8bc3b6ea34c33
sazsu <shonniy@yandex.ru>
2024-11-15 15:38:05 +0300

    feat: add cancel cross button class

--- app/widgets/cancel_cross_button.py
+++ app/widgets/cancel_cross_button.py
@@ -4,8 +4,3 @@ from app.widgets.base_icon_button import BaseIconButton
 class CancelCrossButton(BaseIconButton):
 	def __init__(self, light_icon_path: str, dark_icon_path: str, parent):
 		super().__init__(light_icon_path, dark_icon_path, parent)
-		self.clicked.connect(self.btn_handler)
-
-	def btn_handler(self):
-		# close current window
-		self.parent().close()
