236f0dea3d71a2166a05e974cd5b812b6a8aceb5
sazsu <shonniy@yandex.ru>
2024-11-18 22:15:41 +0300

    feat: add app icons

--- app/main_window.py
+++ app/main_window.py
@@ -1,15 +1,20 @@
 from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtGui import QIcon
 
 from app.config import Config
-from app.local_profile_page import LocalProfilePage
 from app.managers.database_manager import DatabaseManager
-from app.test_page import TestPage
+from app.pages.local_profile_page import LocalProfilePage
+from app.pages.test_page import TestPage
 from app.ui.main_window_ui import Ui_MainWindow
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 	def __init__(self) -> None:
 		super().__init__()
+		self.icon_paths = {
+			True: QIcon('app/assets/logo_light.svg'),
+			False: QIcon('app/assets/logo_dark.svg')
+		}
 		self.setupUi(self)
 
 		self.db_manager = DatabaseManager('app.sqlite3')
@@ -29,6 +34,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		self.setStyleSheet(
 			Config.app_light if self.interface_mode else Config.app_dark
 		)
+		self.setWindowIcon(self.icon_paths[self.interface_mode])
 		self.pages.widget(0).set_mode(self.interface_mode)
 		self.pages.widget(1).set_mode(self.interface_mode)
 
